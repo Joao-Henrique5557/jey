3ca52025c57719bb245abd109543cd35b42e5473
João Henrique da silva <7joaohenrique5557@gmail.com>
2025-12-30 15:02:02 -0300

    Começando no React
    Novas pastas,
    iniciando um projeto multipages,
    primeiras configurações.

--- site-jeffrey/frontend/src/main.jsx
+++ site-jeffrey/frontend/src/main.jsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './styles/global.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
