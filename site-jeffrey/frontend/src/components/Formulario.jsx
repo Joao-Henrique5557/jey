be8c5ee34788af07c9e7c1442a6ff43ffeb3d0bf
João Henrique da silva <7joaohenrique5557@gmail.com>
2026-01-17 15:36:03 -0300

    começando com a função de mudar theme

--- site-jeffrey/frontend/src/components/Formulario.jsx
+++ site-jeffrey/frontend/src/components/Formulario.jsx
@@ -4,6 +4,7 @@ import Btn from "./Btn"
 const Formulario = ({procedimento}) => {
     if(procedimento === "login"){
         return <div id="formulario">
+            <h1>Jeffrey</h1>
             <Input label="E-mail" />
             <Input label="Senha" />
             <Btn />
@@ -12,6 +13,7 @@ const Formulario = ({procedimento}) => {
         </div>
     } else if(procedimento === "cadastro"){
         return <div id="formulario">
+            <h1>Jeffrey</h1>
             <Input label="Nome" />
             <Input label="E-mail" />
             <Input label="Senha" />
--- site-jeffrey/frontend/src/pages/Login.jsx
+++ site-jeffrey/frontend/src/pages/Login.jsx
@@ -1,10 +1,15 @@
 import Formulario from "../components/Formulario"
+import { useState } from "react";
 
 const Login = () => {
+    const [theme, setTheme] = useState("light_mode");
+
     return <div id="login">
         
+
         <Formulario procedimento="login" />
     </div>
 }
+
 
 export default Login
