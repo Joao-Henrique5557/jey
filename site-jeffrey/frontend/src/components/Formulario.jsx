39777950a7d60bbc0435795377657e9bb30989a0
João Henrique da silva <7joaohenrique5557@gmail.com>
2026-02-06 16:38:26 -0300

    + padroes

--- site-jeffrey/frontend/src/components/Formulario.jsx
+++ site-jeffrey/frontend/src/components/Formulario.jsx
@@ -3,20 +3,21 @@ import Btn from "./Btn";
 
 const Formulario = (tipo) => {
     if(tipo.procedimento === "login"){
-        return <div className="formulario">
+        return <form className="formulario">
             <h1>Jeffrey - Login</h1>
             <Input label="E-mail" />
             <Input label="Senha" />
             <Btn />
-        </div>
+        </form>
+
     } else if(tipo.procedimento === "cadastro"){
-        return <div className="formulario">
+        return <form className="formulario">
             <h1>Jeffrey - Cadastro</h1>
             <Input label="Nome" />
             <Input label="E-mail" />
             <Input label="Senha" />
             <Btn />
-        </div>
+        </form>
     }
 }
 
